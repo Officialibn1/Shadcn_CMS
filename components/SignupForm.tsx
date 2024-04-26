7355989ececd6b25a3df12ce22ba1857aa00bd2c
Isah Ibn Muhammad <officialibn001@gmail.com>
2024-04-26 08:26:29 +0100

    Initiated zod and hook form in signup page

--- components/SignupForm.tsx
+++ components/SignupForm.tsx
@@ -5,33 +5,108 @@ import { cn } from "@/lib/utils";
 import { Icons } from "@/components/Icons";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "./ui/form";
+import { toast } from "./ui/use-toast";
+
+const signUpFormSchema = z.object({
+	firstName: z
+		.string()
+		.min(3, {
+			message: "First name must be at least 3 characters!",
+		})
+		.max(20, {
+			message: "First name msut not me more than 20 characters!",
+		}),
+	lastName: z
+		.string()
+		.min(3, {
+			message: "Last name must be at least 3 characters!",
+		})
+		.max(20, {
+			message: "Last name msut not me more than 20 characters!",
+		}),
+	email: z.string().email("Enter a valid email address."),
+	password: z
+		.string()
+		.min(8, "Password must be at least 8 characters long")
+		.regex(/[a-z]/, "Password must contain at least one lowercase letter")
+		.regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+		.regex(/\d/, "Password must contain at least one digit")
+		.regex(
+			/[!@#$%^&*(),.?":{}|<>]/,
+			"Password must contain at least one special character",
+		),
+});
+
+type SignUpFormValues = z.infer<typeof signUpFormSchema>;
+
+const defaultSignUpFormValues: Partial<SignUpFormValues> = {
+	firstName: "",
+	lastName: "",
+	email: "",
+	password: "",
+};
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
-const SignUpForm = ({ className, ...props }: UserAuthFormProps) => {
+const SignUpForm = () => {
 	const [isLoading, setIsLoading] = useState(false);
 
-	async function onSubmit(event: React.SyntheticEvent) {
-		event.preventDefault();
-		setIsLoading(true);
+	const form = useForm<SignUpFormValues>({
+		resolver: zodResolver(signUpFormSchema),
+		defaultValues: defaultSignUpFormValues,
+		mode: "onChange",
+	});
 
+	async function onSubmit(data: SignUpFormValues) {
+		setIsLoading(true);
+		toast({
+			title: "You submitted the following values:",
+			description: (
+				<pre className='mt-2 w-[340px] rounded-md bg-slate-950 p-4'>
+					<code className='text-white'>{JSON.stringify(data, null, 2)}</code>
+				</pre>
+			),
+		});
 		setTimeout(() => {
 			setIsLoading(false);
 		}, 3000);
 	}
 
 	return (
-		<div
-			className={cn("grid gap-6", className)}
-			{...props}>
-			<form onSubmit={onSubmit}>
+		<div className={cn("grid gap-6")}>
+			{/* <form onSubmit={onSubmit}>
 				<div className='grid gap-2'>
 					<div className='grid gap-1'>
 						<Label
 							className='sr-only'
 							htmlFor='email'>
-							Full Name
+							First Name
+						</Label>
+						<Input
+							id='name'
+							placeholder='Enter Name'
+							type='text'
+							autoCorrect='off'
+							disabled={isLoading}
+						/>
+					</div>
+
+					<div className='grid gap-1'>
+						<Label
+							className='sr-only'
+							htmlFor='email'>
+							Last Name
 						</Label>
 						<Input
 							id='name'
@@ -81,8 +156,97 @@ const SignUpForm = ({ className, ...props }: UserAuthFormProps) => {
 						SignUp with Email
 					</Button>
 				</div>
-			</form>
-			<div className='relative'>
+			</form> */}
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(onSubmit)}>
+					<FormField
+						control={form.control}
+						name='firstName'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel className='font-light'>First Name</FormLabel>
+								<FormControl>
+									<Input
+										placeholder='Enter First Name'
+										{...field}
+										onChange={field.onChange}
+										defaultValue={field.value}
+									/>
+								</FormControl>
+
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name='lastName'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel className='font-light'>Last Name</FormLabel>
+								<FormControl>
+									<Input
+										placeholder='Enter Last Name'
+										{...field}
+										onChange={field.onChange}
+										defaultValue={field.value}
+									/>
+								</FormControl>
+
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name='email'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel className='font-light'>Email</FormLabel>
+								<FormControl>
+									<Input
+										placeholder='Enter Valid Email'
+										{...field}
+										onChange={field.onChange}
+										defaultValue={field.value}
+									/>
+								</FormControl>
+
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name='password'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel className='font-light'>Password</FormLabel>
+								<FormControl>
+									<Input
+										placeholder='Enter password at least 8 characters'
+										{...field}
+										onChange={field.onChange}
+										defaultValue={field.value}
+									/>
+								</FormControl>
+
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+
+					<Button
+						className='w-full mt-3'
+						disabled={isLoading}>
+						{isLoading && (
+							<Icons.spinner className='mr-2 h-4 w-4 animate-spin' />
+						)}
+						SignUp with Email
+					</Button>
+				</form>
+			</Form>
+			{/* <div className='relative'>
 				<div className='absolute inset-0 flex items-center'>
 					<span className='w-full border-t' />
 				</div>
@@ -91,8 +255,8 @@ const SignUpForm = ({ className, ...props }: UserAuthFormProps) => {
 						Or continue with
 					</span>
 				</div>
-			</div>
-			<Button
+			</div> */}
+			{/* <Button
 				variant='outline'
 				type='button'
 				disabled={isLoading}>
@@ -102,7 +266,7 @@ const SignUpForm = ({ className, ...props }: UserAuthFormProps) => {
 					<Icons.gitHub className='mr-2 h-4 w-4' />
 				)}{" "}
 				GitHub
-			</Button>
+			</Button> */}
 		</div>
 	);
 };
